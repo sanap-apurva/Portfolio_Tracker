6f6db7699389b300fbf78d616e6cb5236a14f979
sanap-apurva <apurvasanap98@gmail.com>
2024-09-28 15:40:13 +0530

    Updated the model to have an incremental ID for trades

--- models/Trade.js
+++ models/Trade.js
@@ -1,6 +1,9 @@
+// models/Trade.js
 const mongoose = require('mongoose');
+const Counter = require('./Counter');
 
 const tradeSchema = new mongoose.Schema({
+  tradeId: { type: Number, unique: true }, // Add the incremental trade ID
   stock: { type: String, required: true },
   quantity: { type: Number, required: true },
   price: { type: Number, required: true },
@@ -8,4 +11,19 @@ const tradeSchema = new mongoose.Schema({
   type: { type: String, enum: ['buy', 'sell'], required: true }
 });
 
-module.exports = mongoose.model('Trade', tradeSchema);
+// Pre-save middleware to auto-increment tradeId
+tradeSchema.pre('save', async function (next) {
+  if (this.isNew) {
+    const counter = await Counter.findOneAndUpdate(
+      { id: 'tradeId' },
+      { $inc: { seq: 1 } },
+      { new: true, upsert: true } // Create a new counter if it doesn't exist
+    );
+    this.tradeId = counter.seq;
+  }
+  next();
+});
+
+const Trade = mongoose.model('Trade', tradeSchema);
+
+module.exports = Trade;
